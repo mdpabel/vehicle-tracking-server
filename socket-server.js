0c5fb7dfbc2c9be48bdc748c9e0de0143a1e16bb
mdpabel <mdpabel385@gmail.com>
2022-07-21 16:24:59 +0600

    calculated the center of the coordinates

--- socket-server.js
+++ socket-server.js
@@ -1,14 +1,29 @@
 const geolib = require('geolib');
 
+function calculateCenterOfCoordinates(coordinates) {
+  const coordinatesValues = [];
+  for (let i = 0; i < coordinates.length; i++) {
+    coordinatesValues.push({
+      latitude: coordinates[i].lattitude,
+      longitude: coordinates[i].longitude,
+    });
+  }
+  return geolib.getCenter(coordinatesValues);
+}
+
 function socketServer(io) {
   return io.on('connection', (socket) => {
     let coordinates = [];
 
     socket.on('gpsdata', (data) => {
       if (coordinates.length >= 10) {
-        const coordinate = geolib.getCenter(coordinates);
+        const coordinate = calculateCenterOfCoordinates(coordinates);
+        coordinate['busId'] = coordinates[0].busId;
+        coordinate['numberOfSatellites'] = coordinates[0].numberOfGPS;
+
         console.log(coordinates, coordinate);
         coordinates = [];
+
         io.emit('gpsdataforclients', coordinate);
       } else {
         coordinates.push(data);
